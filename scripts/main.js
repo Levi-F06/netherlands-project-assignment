593b00ec331e4207dd169b1f6bad8d2ae3f7eed5
Levi-F06 <levi.flanagan06@gmail.com>
2024-10-04 10:55:00 +0100

    :fire: Remove navbar toggler

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-const navbarToggle = document.querySelector("#navbar-toggle");
-const navLinks = document.querySelectorAll(".nav-link");
-const navUl = document.querySelector("header>ul");
-
-navbarToggle.addEventListener("click", () => {
-  Array.from(navLinks).map((link) => link.classList.toggle("nav-active"));
-  navUl.classList.toggle("ul-active");
-  navUl.parentNode.classList.toggle("header-active");
-});
